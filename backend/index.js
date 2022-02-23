bc7f85d0f6bc4e17b80b86297966ac5226156767
Pavan <pavanagrawal@outlook.com>
2022-02-22 22:49:10 -0600

    SUPER BASIC STRUCTURE

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,49 @@
 const express = require('express')
 const app = express()
 const port = 3000
+require("google-closure-library");
+goog.require("goog.structs.PriorityQueue")
+
+var buyOrderBook = new goog.structs.PriorityQueue();
+var sellOrderBook = new goog.structs.PriorityQueue();
+var PNLs = {};
+var Positions = {};
+
+
+function addPosition(person, quantity, price) {
+    if (!Positions[person]) {
+        Positions[person] = []
+    }
+    Positions[person].push({
+        price,quantity
+    })
+}
+
+function matchOrders() {
+    if (buyOrderBook.peekKey() >= sellOrderBook.peekKey()) {
+        const buyOrder = buyOrderBook.dequeue();
+        const sellOrder = sellOrderBook.dequeue();
+        addPosition(buyOrder.person, 1, buyOrder.price)
+        addPosition(sellOrder.person, -1, buyOrder.price)
+        matchOrders();
+    }
+}
+
+app.use(express.json());
+
+app.post('/order', (req, res) => {
+    const {price, type, person} = req.body;
+    switch (type) {
+        case "BUY": 
+            buyOrderBook.enqueue(price, {price, person});
+        break
+        case "SELL":
+            sellOrderBook.enqueue(-price, {price, person});
+    }
+    matchOrders();
+    res.send({bestBid: buyOrderBook.peekKey(), bestAsk: sellOrderBook.peekKey(), Positions});
+    
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
