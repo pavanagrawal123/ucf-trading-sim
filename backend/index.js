413c00814c5801128567e2aaef0c765e6e838ec4
TylerKerch <kerchtc@gmail.com>
2022-02-25 16:21:58 -0600

    :sparkles: Order quantity

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,26 @@ function addPosition(person, quantity, price) {
 }
 
 function matchOrders() {
-    if (buyOrderBook.peekKey() >= sellOrderBook.peekKey()) {
-        const buyOrder = buyOrderBook.dequeue();
-        const sellOrder = sellOrderBook.dequeue();
-        addPosition(buyOrder.person, 1, buyOrder.price)
-        addPosition(sellOrder.person, -1, buyOrder.price)
+    if (-buyOrderBook.peekKey() >= sellOrderBook.peekKey()) {
+        const buyOrder = buyOrderBook.peek();
+        const sellOrder = sellOrderBook.peek();
+        if(buyOrder.quantity == sellOrder.quantity){
+            addPosition(buyOrder.person, buyOrder.quantity, buyOrder.price)
+            addPosition(sellOrder.person, sellOrder.quantity, buyOrder.price)
+            buyOrderBook.dequeue();
+            sellOrderBook.dequeue();
+        }else if(buyOrder.quantity > sellOrder.quantity){
+            addPosition(sellOrder.person, sellOrder.quantity, buyOrder.price);
+            addPosition(buyOrder.person, sellOrder.quantity, buyOrder.price);
+            sellOrderBook.dequeue();
+            buyOrder.quantity -= sellOrder.quantity;
+        }else{
+            addPosition(buyOrder.person, buyOrder.quantity, buyOrder.price);
+            addPosition(sellOrder.person, buyOrder.quantity, buyOrder.price);
+            buyOrderBook.dequeue();
+            sellOrder.quantity -= buyOrder.quantity;
+        }
+        
         matchOrders();
     }
 }
@@ -32,13 +47,13 @@ function matchOrders() {
 app.use(express.json());
 
 app.post('/order', (req, res) => {
-    const {price, type, person} = req.body;
+    const {price, type, person, quantity} = req.body;
     switch (type) {
         case "BUY": 
-            buyOrderBook.enqueue(price, {price, person});
+            buyOrderBook.enqueue(-price, {price, quantity, person});
         break
         case "SELL":
-            sellOrderBook.enqueue(-price, {price, person});
+            sellOrderBook.enqueue(price, {price, quantity, person});
     }
     matchOrders();
     res.send({bestBid: buyOrderBook.peekKey(), bestAsk: sellOrderBook.peekKey(), Positions});
